7e952290a2275aa93bce536dcd0f194604457349
lisa-crowell <lisa.l.crowell@gmail.com>
2022-10-18 19:13:29 -0500

    add NuGet packages and connection string

--- FlashCardService/Program.cs
+++ FlashCardService/Program.cs
@@ -1,6 +1,13 @@
+using FlashCardService.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<FlashCardDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
